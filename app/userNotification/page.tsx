ce66441fbfe4205ef3dbf7e626bfcda1ce61a096
HimanshuHegde <himanshuhegde797@gmail.com>
2024-11-23 14:03:31 +0530

    designed the norification page

--- app/userNotification/page.tsx
+++ app/userNotification/page.tsx
@@ -1,39 +1,57 @@
 "use client";
 import { useSession } from "next-auth/react";
 import { NotificationRetrieve } from "@/backend";
-import { useEffect, useState } from "react";
-import { stat } from "fs";
+import { useEffect, useRef, useState } from "react";
 
 export default function UserNotification() {
+  let temp = useRef<HTMLDivElement>(null);
   let [notInfo, SetNotInfo] = useState<Record<string, string>[]>([]);
   const { data: session, status } = useSession();
   const email = session?.user?.email;
   useEffect(() => {
-    if (status == "authenticated")
+    if (status == "authenticated"){
       (async () => {
         SetNotInfo(await NotificationRetrieve(email || ""));
       })();
+     
+    }
   }, [session]);
-
+useEffect(()=>{
+  if(temp.current){
+    temp.current.scrollTop = temp.current.scrollHeight;
+  }
+},[notInfo])
   return (
-    <>
-      <div>
-
+    <div className="NotiContainer">
+      <div className="NotiDiVHolder" ref={
+        temp
+      }>
         {notInfo.map((e) => {
-          if (e.user == "1")
+          if (e.user == "1"){
+            let splitInfoR = e.info.split(",");
+            let parts = splitInfoR[1].split(':');
+            let time = parts[0]+':'+parts[1]+' '+parts[2].split(' ')[1];
             return (
-              <div className="rightNoti">
-                <p>{e.info}</p>
+              <div className="rightNoti" key={e._id}>
+                <p>{splitInfoR[0]}</p>
+                <p className="Pr">{time}</p>
               </div>
             );
-          else if(e.user == "0")
+          }
+          else if(e.user == "0"){
+            let splitInfoL = e.info.split(",");
+            let parts = splitInfoL[1].split(':');
+            let time = parts[0]+':'+parts[1]+' '+parts[2].split(' ')[1];
           return(
-            <div className="leftNoti">
-              <p>{e.info}</p>
+            <div className="leftNoti" key={e._id}>
+              <p>{splitInfoL[0]}</p>
+              <p className="Pl">{time}</p>
             </div>
-          );
+          )
+        }
         })}
+        <i className="scroll"></i>
       </div>
-    </>
+    </div>
   );
 }
